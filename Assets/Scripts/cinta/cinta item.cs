af82534286bb8ca5d6ad4909d16c0f67b3809286
Dantestriglio <Dantestriglio@gmail.com>
2026-08-11 14:23:03 -0300

    arranco a hacer los scripts

---
+++ Assets/Scripts/cinta/cinta item.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cinta_item : MonoBehaviour
+{
+
+        public GameObject item;
+
+    private void Awake()
+    {
+        item = Gameobject;
+    }
+
+}
+
---
+++ Assets/Scripts/manager/Cinta_Manager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cinta_Manager : MonoBehaviour
+{   
+
+    public float speed = 2f;
+
+}
