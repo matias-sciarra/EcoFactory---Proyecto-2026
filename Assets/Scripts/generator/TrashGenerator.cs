6fdf0099eacb9632ad31e9170dc0cd15e4320d26
Dantestriglio <Dantestriglio@gmail.com>
2026-08-20 12:04:03 -0300

    arranco con el script de generar

---
+++ Assets/Scripts/generator/TrashGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashGenerator : MonoBehaviour
+
+{
+    public GameObject Prefabtrash;
+    private float temporizador;
+    public float intervalo = 2f;
+    public Belt primeraCinta;       
+
+
+
+    void update()
+    {
+        temporizador+= Time.deltaTime;
+        if (temporizador >= intervalo)
+        {
+            temporizador = 0f;
+            GenerateTrash()
+        }
+
+       void GenerateTrash()
+       {
+
+
+       }
+
+    }
+
+
+
+
+}
+
+
