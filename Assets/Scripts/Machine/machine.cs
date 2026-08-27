d9236d59f077b38d243454cc02b6498878e7a424
Dantestriglio <Dantestriglio@gmail.com>
2026-08-27 11:44:44 -0300

    un toque de todo

--- Assets/Scripts/Machine/machine.cs
+++ Assets/Scripts/Machine/machine.cs
@@ -5,11 +5,33 @@ using UnityEngine;
 public class machine : MonoBehaviour
 {
     private int canridadrequerida = 4;
-    public GameObject preresiduo;
     public GameObject residuoproscesado;
     public Belt primeraCinta;
+    private int contador = 0;
 
 
 
-    public void recibirbasura(trash )
+    public void ReceiveTrash(Trash Basura)
+    {
+        contador+=1;
+        Debug.Log("llego bien");
+        Destroy(Basura.gameObject);
+        if (contador >= canridadrequerida){
+            contador = 0;
+            generarbasuraproscesada();
+        }
+    }
+
+    public void generarbasuraproscesada()
+    {
+        Vector3 position = primeraCinta.GetItemPosition();
+        Quaternion rotacion = Quaternion.identity; 
+        GameObject nueva = Instantiate(residuoproscesado, position, rotacion);
+        Debug.Log("produciodo");
+
+        BeltItem itemcomponent = nueva.GetComponent<BeltItem>();
+        primeraCinta.beltItem = itemcomponent;
+
+        Debug.Log("en la cinta");
+    }
 }
