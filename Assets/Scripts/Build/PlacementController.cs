e6ed43e52ffe3d008d4c5e79e92a702bec6f5724
matias-sciarra <Matiassciarra1@gmail.com>
2026-09-03 10:39:41 -0300

    Sistema de construccion

---
+++ Assets/Scripts/Build/PlacementController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementController : MonoBehaviour
+{
+    public GameObject machinePrefab;
+    public Camera cam;
+
+    void Update()
+    {
+        if (!Input.GetMouseButtonDown(0)) return;
+
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit, 100f))
+        {
+            Vector2Int cell = Grid_Manager.Instance.WorldToGrid(hit.point);
+
+            if (!Grid_Manager.Instance.IsCellFree(cell))
+            {
+                Debug.Log("Celda ocupada, no se puede colocar ahí.");
+                return;
+            }
+
+            Vector3 worldPos = Grid_Manager.Instance.GridToWorld(cell);
+            GameObject go = Instantiate(machinePrefab, worldPos, Quaternion.identity);
+
+            MachineBase m = go.GetComponent<MachineBase>();
+            if (m != null) m.gridPosition = cell;
+
+            Grid_Manager.Instance.Occupy(cell, m);
+        }
+    }
+}
