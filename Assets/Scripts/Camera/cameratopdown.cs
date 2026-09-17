5af314ed0610745409fc8633dc957ea5458c0d4d
Dantestriglio <Dantestriglio@gmail.com>
2026-09-17 11:56:20 -0300

    camera

---
+++ Assets/Scripts/Camera/cameratopdown.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameratopdown : MonoBehaviour
+{
+    public GameObject player;
+    public float upset = 5f;
+    void Start()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        getplayerposition();
+    }
+
+    void getplayerposition()
+    {
+        Vector3 playerPosition = player.transform.position;
+        transform.position = new Vector3(playerPosition.x, playerPosition.y + upset, playerPosition.z);
+    }
+}
---
+++ Assets/Scripts/manager/cameramanager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameramanager : MonoBehaviour
+{
+    public Camera fpscamera;
+    public Camera topdown;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            cambiarcamara();
+        }
+    }
+    
+    void cambiarcamara()
+    {
+    bool topDownActiva = topdown.enabled;
+
+    topdown.enabled = !topDownActiva;
+    fpscamera.enabled = topDownActiva;
+    }
+}
